6e173e5da9174daa76926a63da84b6abfaef99ce
Omar <omarucolombari@gmail.com>
2022-09-15 18:01:59 -0300

    Feat: criado component Background e integrado ao app

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ImageBackground } from "react-native";
+import { styles } from "./styles";
+import backgroundImage from "../../assets/background-galaxy.png";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground
+      source={backgroundImage}
+      defaultSource={backgroundImage}
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
